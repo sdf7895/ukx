e2fe55c7c32126cda92b487d639ba34072d3a62f
sdf7895@naver.com <sdf7895@naver.com>
2023-11-07 23:08:51 +0900

    chore : init

---
+++ lib/style/border/border.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class TotalBorders {
+  static const BorderSide primaryBorder = BorderSide(
+    color: Colors.grey,
+    width: 2.0,
+    style: BorderStyle.solid,
+  );
+
+  static const BorderRadius primaryRadius = BorderRadius.all(
+    Radius.circular(10.0),
+  );
+}
---
+++ lib/style/colors/total.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class TotalColors {
+  static const Color primaryColor = Color(0xFF6200EE); // 미드나잇 블루
+  static const Color secondaryColor = Color(0xFF18D620); // 그린
+  static const Color accentColor = Color(0xFFFFD500); // 아마존
+  static const Color customColor = Color(0xFFD00000); // 커스텀 컬러
+
+  // Material Design Colors
+  static const Color red = Colors.red;
+  static const Color blue = Colors.blue;
+  static const Color green = Colors.green;
+  static const Color yellow = Colors.yellow;
+  static const Color orange = Colors.orange;
+  static const Color purple = Colors.purple;
+  static const Color pink = Colors.pink;
+  static const Color teal = Colors.teal;
+  static const Color cyan = Colors.cyan;
+
+  // 기타 색상
+  static const Color amber = Colors.amber;
+  static const Color brown = Colors.brown;
+  static const Color grey = Colors.grey;
+  static const Color indigo = Colors.indigo;
+  static const Color lime = Colors.lime;
+  static const Color deepPurple = Colors.deepPurple;
+  static const Color deepOrange = Colors.deepOrange;
+  static const Color lightBlue = Colors.lightBlue;
+  static const Color lightGreen = Colors.lightGreen;
+}
