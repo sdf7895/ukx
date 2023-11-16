b700d3b0e476e760368135a6b9fa16441d16e1ed
sdf7895@naver.com <sdf7895@naver.com>
2023-11-17 00:13:13 +0900

    chore : tabs 분리

--- lib/options/tabs/default.dart
+++ lib/options/tabs/default.dart
@@ -1,10 +1,23 @@
 import 'package:flutter/material.dart';
 
-class DefaultTabs extends StatelessWidget {
-  const DefaultTabs({super.key});
+import '../../style/text/tabs-style.dart';
+import '../../texts/strings.dart';
 
-  @override
-  Widget build(BuildContext context) {
-    return const Placeholder();
-  }
+class DefaultTabs {
+  static List<Tab> tabs = [
+    Tab(
+      child: Text(
+        AppBarDefaultTabBar.defatulOne,
+        style: TabStyles.defaultTab,
+      ),
+    ),
+    Tab(
+      child: Text(
+        AppBarDefaultTabBar.defaultTwo,
+        style: TabStyles.defaultTab,
+      ),
+    )
+  ];
+
+  DefaultTabs();
 }
